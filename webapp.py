238307a7c94f6e5eccb61bb775abbcdddb4a4ef2
odelola_solomon <odelolasolomon5@gmail.com>
2024-05-23 17:15:32 +0100

    corrceted the st.text_inputs for the variables

--- webapp.py
+++ webapp.py
@@ -3,8 +3,7 @@ import pickle
 import streamlit as st 
 
 # Load the model
-model_path = 'predictor.sav'  
-# Update the path to your model file
+model_path = 'predictor.sav'
 loaded_model = pickle.load(open(model_path, 'rb'))
 
 # Creating a function for Prediction
@@ -15,34 +14,31 @@ def completionrate_prediction(input_data):
     # Reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
     prediction = loaded_model.predict(input_data_reshaped)
-    
-    print('the predicted completion rate is {}'.format(prediction))
+    return prediction
 
 def main():
     # Giving a title
-    st.title('Completion Rate Prediction Application')
+    st.title('School Completion Rate Prediction Application')
 
-    
     # Getting the input data from the user
-    ptwmrseagpi = st.text_input('ptwmrseagpi', '0.0')
-    ppels_2_cg = st.text_input('ppels_2_cg', '0.0')
-    ofsussa = st.text_input('ofsussa', '0.0')
-    earcuse_twnetyfive = st.text_input('earcuse_twnetyfive', '0.0')
-    pseb_12lse = st.text_input('pseb_12lse', '0.0')
-    naspi = st.text_input('naspi', '0.0')
-    earclshp_twentyfive = st.text_input('earclshp_twentyfive', '0.0')
-    pspmprbs = st.text_input('pspmprbs', '0.0')
-    gils = st.text_input('gils', '0.0')
-    ofspsa = st.text_input('ofspsa', '0.0')
-    ofspsa = st.text_input('ofspsa', '0.0')
-    
-    
+    pspmlsrbs = st.text_input('pspmlsrbs', '0.0')
+    gip = st.text_input('gip', '0.0')
+    ofsalssa = st.text_input('ofsalssa', '0.0')
+    ppp_2_cg = st.text_input('ppp_2_cg', '0.0')
+    pc_24_59_thlp = st.text_input('pc_24_59_thlp', '0.0')
+    nerece = st.text_input('nerece', '0.0')
+    nerpp = st.text_input('nerpp', '0.0')
+    pyalmlpdl = st.text_input('pyalmlpdl', '0.0')
+    earcpehp_twentyfiveplus = st.text_input('earcpehp_twentyfiveplus', '0.0')
+    ptmrepeagpi = st.text_input('ptmrepeagpi', '0.0')
+    geeduv = st.text_input('geeduv', '0.0')
+    pcypepfm = st.text_input('pcypepfm', '0.0')
+    pcypelsfr = st.text_input('pcypelsfr', '0.0')
+
     # Code for Prediction
     if st.button('Predict school completion rate'):
-        prediction = completionrate_prediction(['ptwmrseagpi', 'ppels_2_cg',
-                                              'ofsussa','earcuse_twnetyfive','pseb_12lse',
-                                        'naspi','earclshp_twentyfive','pspmprbs','gils',
-                         'ofspsa', 'pcypesfm'])
+        input_data = [pspmlsrbs, gip, ofsalssa, ppp_2_cg, pc_24_59_thlp, nerece, nerpp, pyalmlpdl, earcpehp_twentyfiveplus, ptmrepeagpi, geeduv, pcypepfm, pcypelsfr]
+        prediction = completionrate_prediction(input_data)
         st.success(f'Predicted school completion rate is : {prediction}')
 
 if __name__ == '__main__':
